cde6b1bfc7047d5fdada751897d0ac431e62a014
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-10-06 12:39:22 +0200

    added functionality for create and save tutorial

---
+++ nodejs-express-mongodb/app/controllers/tutorial.controller.js
@@ -0,0 +1,57 @@
+const db = require('../models');
+const Tutorial = db.tutorials;
+
+// Create and Save a new tutorial
+exports.create = (req, res) => {
+    // validate request
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content cannot be empty!" });
+        return;
+    }
+
+    // create a tutorial
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    });
+
+    // save tutorial
+    tutorial.save(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message || "Some error occured while creating the tutorial"});
+        });
+};
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+};
