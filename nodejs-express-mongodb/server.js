b703dd2cb664ff4cd8a3757492393a7008fee1f0
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-31 10:23:29 +0200

    update

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 var corsOptions = {
-    origin: 'http://localhost:0801'
+    origin: 'http://localhost:8081'
 };
 
 app.use(cors(corsOptions));
